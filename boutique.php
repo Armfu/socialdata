dd8fc8b84e7a452270f4854d0d6d609145214c36
Socdam.com <89734882+Armfu@users.noreply.github.com>
2023-07-22 12:25:15 +0000

    Update boutique.php

--- boutique.php
+++ boutique.php
@@ -1,5 +1,5 @@
 <?php
-require_once("inc/init.php");
+require_once("init.php");
 //--------------------------------- TRAITEMENTS PHP ---------------------------------//
 //--- AFFICHAGE DES CATEGORIES ---//
 $categories_des_produits = executeRequete("SELECT DISTINCT categorie FROM produit");
@@ -28,6 +28,6 @@ if(isset($_GET['categorie']))
 }
 $contenu .= '</div>';
 //--------------------------------- AFFICHAGE HTML ---------------------------------//
-require_once("inc/haut.php");
+require_once("haut.php");
 echo $contenu;
-require_once("inc/bas.inc.php");
+require_once("bas.inc.php");
